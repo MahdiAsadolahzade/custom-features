af49870b936ad85170eb957dc5ef44d67e3eef0f
MahdiAsadolahzade <mahdiasadi140@gmail.com>
2024-03-09 11:44:52 +0330

    changes on tooltip show

--- src/app/pages/Tooltip/page.tsx
+++ src/app/pages/Tooltip/page.tsx
@@ -5,13 +5,31 @@ type Props = {};
 
 const TryTooltip = (props: Props) => {
   const positions = [
-    "top-left", "top", "top-right",
-    "left", "center", "right",
-    "bottom-left", "bottom", "bottom-right"
+    "top-left",
+    "top",
+    "top-right",
+    "left",
+    "center",
+    "right",
+    "bottom-left",
+    "bottom",
+    "bottom-right",
   ];
 
-  const renderTooltipButtons = (position, arrow = false) => (
-    <Tooltip key={position} text="this is an example" position={position} arrow={arrow}>
+  const colors = [
+    "#FF5733",
+    "#33FF57",
+    "#5733FF",
+    "#FFFF33",
+    "#33FFFF",
+    "#FF33FF",
+    "#FF3333",
+    "#33FF33",
+    "#3333FF",
+  ];
+
+  const renderTooltipButtons = (position:any, arrow = false, bgColor:any) => (
+    <Tooltip key={position} text="this is an example" position={position} arrow={arrow} bgColor={bgColor}>
       <button className="button">{position}</button>
     </Tooltip>
   );
@@ -19,13 +37,21 @@ const TryTooltip = (props: Props) => {
   return (
     <div className="page-container">
       <div className="grid-container">
-        {positions.map((position) => renderTooltipButtons(position))}
+        {positions.map((position) => renderTooltipButtons(position,false,'#555'))}
       </div>
 
-      <div style={{ margin: '50px' }}>-------------------</div>
+      <div style={{ margin: "50px" }}>-------------------</div>
 
       <div className="grid-container">
-        {positions.map((position) => renderTooltipButtons(position, true))}
+        {positions.map((position) => renderTooltipButtons(position, true,'#555'))}
+      </div>
+
+      <div style={{ margin: "50px" }}>-------------------</div>
+
+      <div className="grid-container">
+        {positions.map((position, index) =>
+          renderTooltipButtons(position, true, colors[index])
+        )}
       </div>
     </div>
   );
--- src/customs/Tooltip.tsx
+++ src/customs/Tooltip.tsx
@@ -8,14 +8,15 @@ interface TooltipProps {
   children: ReactNode;
   position?: 'top' | 'bottom' | 'center' | 'left' | 'right' | 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right';
   arrow?: boolean;
+  bgColor?: string; 
 }
 
-const Tooltip: React.FC<TooltipProps> = ({ text, children, position = 'bottom', arrow = false }) => {
+const Tooltip: React.FC<TooltipProps> = ({ text, children, position = 'bottom', arrow = false, bgColor='#555' }) => {
   return (
     <div className="tooltip-container">
-      <div className={`tooltip ${position} ${arrow ? 'arrow' : ''}`}>
+      <div className={`tooltip ${position} ${arrow ? 'arrow' : ''}`} style={{ backgroundColor: bgColor }}>
         {text}
-        {arrow && <div className={`arrow ${position === 'center' ? 'hidden' : ''}`} />}
+        {arrow && <div className={`arrow ${position === 'center' ? 'hidden' : ''}`}  />}
       </div>
       {children}
     </div>
